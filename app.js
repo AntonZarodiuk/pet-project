521c72bfb201c927da58acd541c179dbfdad3f90
AntonZarodiuk <anton.zarodyuk@gmail.com>
2021-08-21 20:45:10 +0300

    alpha version 4th deploy

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(path.join(__dirname, '/public')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.use(cors());
@@ -25,7 +26,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./config/passport')(passport);
 app.use('/account', router);
-app.use(express.static(path.join(__dirname, 'public')))
 
 Array.prototype.max = function() {
     return Math.max.apply(null, this);
